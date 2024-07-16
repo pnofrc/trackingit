193c8232857b644071495e4ac62ff1d2ed0d987b
Federico Poni <poni@macchinone.local>
2024-07-16 14:40:56 +0200

    dio

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\GeoJsonData;
+use Illuminate\Support\Facades\DB;
+
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -20,5 +22,26 @@ class DatabaseSeeder extends Seeder
             'email' => 'admin@tracking.it',
             'password' => 'password'
         ]);
+
+
+        $filePath = '/var/www/vhosts/gifted-borg.217-160-114-177.plesk.page/httpdocs/public/datasets/comuni.geojson';
+        $contents = file_get_contents($filePath);
+        $geojson = json_decode($contents, true);
+
+        foreach ($geojson['features'] as $feature) {
+            // Assuming 'name' and 'geometry' properties in GeoJSON
+            $name = $feature['properties']['name'];
+            $geometry = json_encode($feature['geometry']);
+
+            // Store GeoJSON data in database
+            GeoJsonData::create([
+                'name' => $name,
+                'geojson' => $geometry,
+                'geom' => DB::raw("ST_GeomFromGeoJSON('$geometry')")
+            ]);
+        }
+
+
+        
     }
 }
