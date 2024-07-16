e9d59452eeacb07688a04fb28b8f4cd016e9f15b
Federico Poni <poni@macchinone.local>
2024-07-16 15:44:29 +0200

    test front render

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,25 +23,5 @@ class DatabaseSeeder extends Seeder
             'password' => 'password'
         ]);
 
-
-        $filePath = '/var/www/vhosts/gifted-borg.217-160-114-177.plesk.page/httpdocs/public/datasets/comuni.geojson';
-        $contents = file_get_contents($filePath);
-        $geojson = json_decode($contents, true);
-
-        foreach ($geojson['features'] as $feature) {
-            // Assuming 'name' and 'geometry' properties in GeoJSON
-            $name = $feature['properties']['name'];
-            $geometry = json_encode($feature['geometry']);
-
-            // Store GeoJSON data in database
-            GeoJsonData::create([
-                'name' => $name,
-                'geojson' => $geometry,
-                'geom' => DB::raw("ST_GeomFromGeoJSON('$geometry')")
-            ]);
-        }
-
-
-        
     }
 }
