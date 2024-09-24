680d55e7123ee8543c06c68a21bb038fa0fe2d16
poni <poni@habitattt.it>
2024-09-24 20:34:56 +0200

    instructions

--- resources/views/filament/pages/istruzioni.blade.php
+++ resources/views/filament/pages/istruzioni.blade.php
@@ -1,12 +1,103 @@
 <x-filament-panels::page>
 
-<style>
-    img{
-        width:15%
-    }
-</style>
 
-<img src="/diocane.png" alt="">
+
+<p>Questa è la pagina delle istruzioni per la gestione della piattaforma TRACKING-IT</p>
+
+
+
+<p>A sinistra vedrai una colonna con le diverse gestioni per le pagine visibili.</p>
+
+
+
+
+<p>Appena verranno salvati i dati saranno automaticamente pubblicati.</p>
+
+
+<p>
+Per ogni sezione si potranno eliminare i vari dati tramite il pulsante "Elimina” </p>
+
+<img src="/instructions/elimina.png">
+
+
+<p>
+Stessa cosa per modificare.</p>
+
+<br>
+<img src="/instructions/edit_elimina.png">
+
+
+<h2>Gestione Ultime Notizie</h2>
+
+<p>
+"Gestione Ultime Notizie” è per pubblicare frasi di aggiornamento che saranno visualizzatili nella pagina principale (http://tr.acking.it) sulla barra in alto. 
+</p>
+
+<p>In alto a destra puoi cliccare su "Nuovo”, si aprirà una pagina dove potrai mettere il Titolo (Title) e l’effettiva frase (Latests).
+</p>
+
+<img src="/instructions/latests.png">
+
+
+<p>Quindi, clicca "Salva”. Puoi anche cliccare "Salva & nuovo” per salvare e scrivere un’altra notizia.
+</p>
+
+<h2>Dataset Comuni e Dataset aree SLL</h2>
+
+<p>
+"Dataset Comuni” è la pagina per caricare i dataset per i comuni invece "Dataset aree SLL” è la pagina per caricare i database per le aree SLL.
+Entrambe funzionano nello stesso modo.
+
+</p>
+
+<p>
+Clicca "Nuovo”, quindi si aprirà sempre la pagina dove potrai scrivere il titolo (Title) e caricare il file (File).</p>
+
+<img src="/instructions/edit-dataset.png">
+
+
+
+<p>Quindi sarà così:</p>
+
+<img src="/instructions/dataset.png">
+
+
+<p>Per ogni dataset ci sono due opzioni, puoi scaricare il dataset (Download Dataset) e puoi fare il "Parse”. Il parsing caricherà i dati di quel dataset nel database della piattaforma, così saranno i dati visualizzati nella mappa della pagina dashboard (http://tr.acking.it/dashboard)</p>
+
+
+
+
+<h2>Gestione Indicatori</h2>
+
+<p>
+Gli indicatori sono le data visualisation che saranno visibili nella pagina Indicatori (http://tr.acking.it/indicatori).
+Clicchi su Nuovo e avrai una pagina dove mettere il Nome (name) del dataset, la Descrizione (description), caricare l’immagine tremar (Visualisation treemap) e l’immagine scatterplot (Visualisation scatterplot).
+</p>
+
+<img src="/instructions/indicatori.png">
+
+
+
+<h2>Gestione Materiali</h2>
+
+<p>
+I Materiali sono gli articoli del blog  (http://tr.acking.it/materiali). Metterai un Titolo (title), un Autore (author), il Contenuto (content) dell’articolo, la data (date), l’immagine (picture) e infine il PDF.</p>
+
+<img src="/instructions/materiali.png">
+
+
+
+<h2>Gestione News</h2>
+
+<p>
+Funziona come la pagina materiali ma sarà applicabile il tipo della News (news type) e potrai selezionare tra "Evento”, "Pubblicazione” ed "altro”; la città dell’evento (city), il collegamento per un sito web esterno.</p>
+
+<img src="/instructions/news.png">
+
+
+
+
+
+
 
 </x-filament-panels::page>
-
